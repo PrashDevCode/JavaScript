b00f07e533be50ca3c8e5fe03bedb76f7082d9e3
Your Name <prashantsinghberath@gmail.com>
2025-11-05 19:59:22 +0530

    js code

---
+++ JS Module-1/printEven.js
@@ -0,0 +1,10 @@
+let e=0;
+for(;e<=20;e+=2){
+    console.log(e);
+} 
+
+for(let i=1;i<=20;i++) {
+    if(i%2==0) {
+        console.log(i);
+    }
+}
---
+++ JS Module-1/vote.js
@@ -0,0 +1,6 @@
+let a=17;
+if(a>=18) {
+    console.log("You can vote");
+} else {
+    console.log("You can't vote");
+}
